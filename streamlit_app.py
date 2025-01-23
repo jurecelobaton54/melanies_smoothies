84c9be79991aa7dcb82c5f55c996651dfcb66d27
jurecelobaton54 <jurece.lobaton@accenture.com>
2025-01-23 22:48:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ session = cnx.session()
 # session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # Convert the Snowpark Dataframe into a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()

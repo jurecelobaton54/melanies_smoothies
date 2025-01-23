ace9ec13ef6f766734cc744dd3c7e3d5aabd2315
jurecelobaton54 <jurece.lobaton@accenture.com>
2025-01-23 22:42:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if ingredients_list:
 
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         
         # New section to display smoothiefront nutrition information
